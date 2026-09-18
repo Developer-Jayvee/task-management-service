466d95760cb495b7da55193e7d5d6493bfb5a941
Jayvee.Dev <hidlaojayvee18@gmail.com>
2026-09-18 20:34:43 +0800

    chore: api code format

--- routes/api.php
+++ routes/api.php
@@ -8,24 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->middleware('throttle:60,1')->group(function () {
-    Route::get('/identity-check', function (Request $request) {
-        if (!in_array($_GET['slug'], ['login']) && request()->user()->getTenant() !== $_GET['slug']) {
-            return response()->json([
-                'message' => 'Unauthenticated',
-                'status' => false,
-                'data' => null
-            ],401);
-        }
 
-        return response()->json([
-            'message' => 'Authenticated',
-            'status' => true,
-            'data' => [
-                'user' => request()->user(),
-                'slug' => request()->user()->getTenant(),
-            ],
-        ]);
-    })->middleware('login.verify');
     Route::prefix('auth')->group(function () {
         Route::post('login', [AuthController::class, 'signIn'])->name('signIn');
         Route::post('register', [AuthController::class, 'signUp'])->name('signUp');
@@ -42,4 +25,22 @@ Route::prefix('v1')->middleware('throttle:60,1')->group(function () {
         Route::patch('ticket-status/{ticket}', [TicketController::class, 'updateStatus']);
     });
 
+    Route::get('/identity-check', function (Request $request) {
+        if (! in_array($_GET['slug'], ['login']) && request()->user()->getTenant() !== $_GET['slug']) {
+            return response()->json([
+                'message' => 'Unauthenticated',
+                'status' => false,
+                'data' => null,
+            ], 401);
+        }
+
+        return response()->json([
+            'message' => 'Authenticated',
+            'status' => true,
+            'data' => [
+                'user' => request()->user(),
+                'slug' => request()->user()->getTenant(),
+            ],
+        ]);
+    })->middleware('login.verify');
 });
