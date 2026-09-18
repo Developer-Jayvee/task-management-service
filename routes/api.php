45dfb0926e8a6e0be6b048a12c673e7f3da0b77f
Jayvee.Dev <hidlaojayvee18@gmail.com>
2026-09-18 20:33:45 +0800

    Feat: slug checker

--- routes/api.php
+++ routes/api.php
@@ -7,34 +7,39 @@ use App\Http\Controllers\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-
 Route::prefix('v1')->middleware('throttle:60,1')->group(function () {
     Route::get('/identity-check', function (Request $request) {
+        if (!in_array($_GET['slug'], ['login']) && request()->user()->getTenant() !== $_GET['slug']) {
+            return response()->json([
+                'message' => 'Unauthenticated',
+                'status' => false,
+                'data' => null
+            ],401);
+        }
+
         return response()->json([
             'message' => 'Authenticated',
             'status' => true,
             'data' => [
                 'user' => request()->user(),
-                'slug' => request()->user()->getTenant()
-            ]
+                'slug' => request()->user()->getTenant(),
+            ],
         ]);
     })->middleware('login.verify');
-    Route::prefix('auth')->group(function() {
-        Route::post('login',[AuthController::class, 'signIn'])->name('signIn');
-        Route::post('register',[AuthController::class, 'signUp'])->name('signUp');
-        Route::get('link/verify',[InvitationLinkController::class,'verify'])->name('verify.link');
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'signIn'])->name('signIn');
+        Route::post('register', [AuthController::class, 'signUp'])->name('signUp');
+        Route::get('link/verify', [InvitationLinkController::class, 'verify'])->name('verify.link');
 
-        Route::middleware('login.verify')->get('logout',[AuthController::class,'signOut'])->name('signOut');
+        Route::middleware('login.verify')->get('logout', [AuthController::class, 'signOut'])->name('signOut');
     });
 
     Route::middleware('login.verify')->group(function () {
-        Route::get('link/generate',[InvitationLinkController::class, 'generate'])->name('generate.link');
+        Route::get('link/generate', [InvitationLinkController::class, 'generate'])->name('generate.link');
 
-        Route::apiResource('project',ProjectController::class);
-        Route::apiResource('ticket',TicketController::class);
-        Route::patch('ticket-status/{ticket}',[TicketController::class, 'updateStatus']);
+        Route::apiResource('project', ProjectController::class);
+        Route::apiResource('ticket', TicketController::class);
+        Route::patch('ticket-status/{ticket}', [TicketController::class, 'updateStatus']);
     });
-    
+
 });
